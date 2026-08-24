373910301b98c44ea068ace0b2a44bb8d7e3079f
LNU, Sharath <sharlnu@iu.edu>
2026-08-24 17:39:01 -0400

    CDK stack — S3, DynamoDB, SNS, SQS with DLQ

---
+++ infra/lib/jdfit-stack.ts
@@ -0,0 +1,57 @@
+import * as cdk from 'aws-cdk-lib/core';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export class JDFitStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'ResumeUploads', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new dynamodb.Table(this, 'Resumes', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'resumeId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new dynamodb.Table(this, 'Analyses', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'analysisId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const textractCompletion = new sns.Topic(this, 'TextractCompletion', {
+      topicName: 'textract-completion',
+    });
+
+    const processDlq = new sqs.Queue(this, 'ProcessDlq', {
+      queueName: 'process-dlq',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, //added
+    });
+
+    const processQueue = new sqs.Queue(this, 'ProcessQueue', {
+      queueName: 'process-queue',
+      visibilityTimeout: cdk.Duration.seconds(90),
+      removalPolicy: cdk.RemovalPolicy.DESTROY, //added
+      deadLetterQueue: {
+        queue: processDlq,
+        maxReceiveCount: 3,
+      },
+    });
+
+    textractCompletion.addSubscription(
+      new snsSubscriptions.SqsSubscription(processQueue),
+    );
+  }
+}
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,17 +1,85 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infra from '../lib/infra-stack';
+import * as cdk from 'aws-cdk-lib/core';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import { JDFitStack } from '../lib/jdfit-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('storage and messaging layer', () => {
+  const app = new cdk.App();
+  const stack = new JDFitStack(app, 'TestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.resourceCountIs('AWS::S3::Bucket', 1);
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    BucketEncryption: {
+      ServerSideEncryptionConfiguration: [
+        {
+          ServerSideEncryptionByDefault: {
+            SSEAlgorithm: 'AES256',
+          },
+        },
+      ],
+    },
+    PublicAccessBlockConfiguration: {
+      BlockPublicAcls: true,
+      BlockPublicPolicy: true,
+      IgnorePublicAcls: true,
+      RestrictPublicBuckets: true,
+    },
+  });
+  template.hasResource('AWS::S3::Bucket', {
+    DeletionPolicy: 'Delete',
+    UpdateReplacePolicy: 'Delete',
+  });
+
+  template.resourceCountIs('AWS::DynamoDB::Table', 2);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    BillingMode: 'PAY_PER_REQUEST',
+    KeySchema: [
+      { AttributeName: 'userId', KeyType: 'HASH' },
+      { AttributeName: 'resumeId', KeyType: 'RANGE' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'userId', AttributeType: 'S' },
+      { AttributeName: 'resumeId', AttributeType: 'S' },
+    ],
+  });
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    BillingMode: 'PAY_PER_REQUEST',
+    KeySchema: [
+      { AttributeName: 'userId', KeyType: 'HASH' },
+      { AttributeName: 'analysisId', KeyType: 'RANGE' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'userId', AttributeType: 'S' },
+      { AttributeName: 'analysisId', AttributeType: 'S' },
+    ],
+  });
+  template.allResources('AWS::DynamoDB::Table', {
+    DeletionPolicy: 'Delete',
+    UpdateReplacePolicy: 'Delete',
+  });
+
+  template.resourceCountIs('AWS::SNS::Topic', 1);
+  template.hasResourceProperties('AWS::SNS::Topic', {
+    TopicName: 'textract-completion',
+  });
+
+  template.resourceCountIs('AWS::SQS::Queue', 2);
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    QueueName: 'process-dlq',
+  });
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    QueueName: 'process-queue',
+    VisibilityTimeout: 90,
+    RedrivePolicy: {
+      deadLetterTargetArn: Match.anyValue(),
+      maxReceiveCount: 3,
+    },
+  });
+
+  template.hasResourceProperties('AWS::SNS::Subscription', {
+    Protocol: 'sqs',
+  });
+
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 0);
+  template.resourceCountIs('AWS::Cognito::UserPool', 0);
 });
